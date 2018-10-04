c185358ebc5bf29f585b4b0001286012f03a5e06
Rudi Kershaw <rudi.kershaw@cognitran.com>
2018-10-04 20:09:53 +0100

    Rudi Kershaw - Initialise maven plugin archetype.

---
+++ src/main/java/com/rudikershaw/gitbuildhook/MyMojo.java
@@ -0,0 +1,40 @@
+package com.rudikershaw.gitbuildhook;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Goal which touches a timestamp file.
+ */
+@Mojo(name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class MyMojo extends AbstractMojo {
+    /**
+     * Location of the file.
+     */
+    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
+    private File outputDirectory;
+
+    public void execute() throws MojoExecutionException {
+        File f = outputDirectory;
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+
+        File touch = new File( f, "touch.txt" );
+        try (final FileWriter w = new FileWriter(touch)) {
+            w.write( "touch.txt" );
+        }
+        catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        }
+    }
+}
