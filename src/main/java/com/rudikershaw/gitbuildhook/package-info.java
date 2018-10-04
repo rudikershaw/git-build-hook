9730788899d6ffd036c326adb0c309e1d5aad581
Rudi Kershaw <rudi.kershaw@cognitran.com>
2018-10-04 21:33:04 +0100

    Rudi Kershaw - Add checkstyle build plugin and customise checkstyle rules.

---
+++ src/main/java/com/rudikershaw/gitbuildhook/package-info.java
@@ -0,0 +1,2 @@
+/** The application's main package where the plugins execute method runs. */
+package com.rudikershaw.gitbuildhook;
--- src/test/java/com/rudikershaw/gitbuildhook/MyMojoTest.java
+++ src/test/java/com/rudikershaw/gitbuildhook/MyMojoTest.java
@@ -1,13 +1,15 @@
 package com.rudikershaw.gitbuildhook;
 
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.WithoutMojo;
-
 import org.junit.Rule;
-import static org.junit.Assert.*;
 import org.junit.Test;
-import java.io.File;
 
 public class MyMojoTest
 {
@@ -15,7 +17,7 @@ public class MyMojoTest
     public MojoRule rule = new MojoRule()
     {
         @Override
-        protected void before() throws Throwable 
+        protected void before() throws Throwable
         {
         }
 
@@ -29,24 +31,15 @@ public class MyMojoTest
      * @throws Exception if any
      */
     @Test
-    public void testSomething()
-            throws Exception
+    public void testSomething() throws Exception
     {
-        File pom = new File( "target/test-classes/project-to-test/" );
-        assertNotNull( pom );
-        assertTrue( pom.exists() );
+        File pom = new File("target/test-classes/project-to-test/");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
 
-        MyMojo myMojo = ( MyMojo ) rule.lookupConfiguredMojo( pom, "touch" );
-        assertNotNull( myMojo );
+        MyMojo myMojo = (MyMojo) rule.lookupConfiguredMojo(pom, "test");
+        assertNotNull(myMojo);
         myMojo.execute();
-
-        File outputDirectory = ( File ) rule.getVariableValueFromObject( myMojo, "outputDirectory" );
-        assertNotNull( outputDirectory );
-        assertTrue( outputDirectory.exists() );
-
-        File touch = new File( outputDirectory, "touch.txt" );
-        assertTrue( touch.exists() );
-
     }
 
     /** Do not need the MojoRule. */
@@ -54,7 +47,7 @@ public class MyMojoTest
     @Test
     public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
 
 }
