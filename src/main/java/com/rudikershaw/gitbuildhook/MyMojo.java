69b909065139ae8d5a70ebab410f92bfd056c601
Rudi Kershaw <rudi.kershaw@cognitran.com>
2018-10-06 21:35:49 +0100

    Rudi Kershaw - Add prototype check that project using plugin is managed by git.

--- src/main/java/com/rudikershaw/gitbuildhook/MyMojo.java
+++ src/main/java/com/rudikershaw/gitbuildhook/MyMojo.java
@@ -1,18 +1,24 @@
 package com.rudikershaw.gitbuildhook;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.eclipse.jgit.api.Git;
 
 /** Starting location for the Git Build Hook plugin and home of the execute method. */
-@Mojo(name = "test", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+@Mojo(name = "check", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class MyMojo extends AbstractMojo {
 
     @Override
     public void execute() throws MojoFailureException {
-        getLog().info("#######################################################");
-        getLog().info("#######################################################");
-        getLog().info("#######################################################");
+        try (final Git git = Git.open(Paths.get(".").toFile())) {
+            getLog().info("Found the local git repo.");
+        } catch (IOException e) {
+            getLog().error("Could not find the local git repository. A git repository is required for this build.", e);
+        }
     }
 }
