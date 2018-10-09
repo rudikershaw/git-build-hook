80f7f26b4651f0cf20878352de61ce7c92432e14
Rudi Kershaw <rudi.kershaw@cognitran.com>
2018-10-09 16:42:42 +0100

    Rudi Kershaw - Add initialise variable to init git repo if one doesn't exist.

--- src/main/java/com/rudikershaw/gitbuildhook/GitBuildHookMojo.java
+++ src/main/java/com/rudikershaw/gitbuildhook/GitBuildHookMojo.java
@@ -9,11 +9,16 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 /** Starting location for the Git Build Hook plugin and home of the execute method. */
 @Mojo(name = "check", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class GitBuildHookMojo extends AbstractMojo {
+
+    /** Whether to initialise a git repository if one does not already exist. */
+    @Parameter(readonly = true)
+    private boolean initialise;
 
     /** Injected MavenProject containing project related information such as base directory. */
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
@@ -29,8 +34,23 @@ public class GitBuildHookMojo extends AbstractMojo {
             } catch (final IOException e) {
                 failBuildBecauseRepoCouldNotBeFound(e);
             }
+        } else if (initialise) {
+            initialiseGitRepository();
         } else {
             failBuildBecauseRepoCouldNotBeFound(null);
+        }
+    }
+
+    /**
+     * Initialise a new git repository.
+     *
+     * @throws MojoFailureException to fail the build and with details of the failure.
+     */
+    private void initialiseGitRepository() throws MojoFailureException {
+        try {
+            Git.init().setDirectory(project.getBasedir()).call();
+        } catch (final GitAPIException e) {
+            failBuildBecauseRepoCouldNotBeFound(e);
         }
     }
 
@@ -42,7 +62,7 @@ public class GitBuildHookMojo extends AbstractMojo {
      * @throws MojoFailureException to fail the build and with details of the failure.
      */
     private void failBuildBecauseRepoCouldNotBeFound(final Exception e) throws MojoFailureException {
-        final String message = "Could not find the local git repository. A git repository is required for this build.";
+        final String message = "Could not find or initialise a local git repository. A repository is required.";
         throw new MojoFailureException(message, e);
     }
 }
--- src/test/java/com/rudikershaw/gitbuildhook/GitBuildHookMojoTest.java
+++ src/test/java/com/rudikershaw/gitbuildhook/GitBuildHookMojoTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.apache.maven.it.Verifier;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.junit.Rule;
@@ -18,28 +19,19 @@ import org.junit.rules.TemporaryFolder;
 public class GitBuildHookMojoTest {
 
     @Rule
-    public MojoRule rule = new MojoRule() {
-        @Override
-        protected void before() throws Throwable {
-
-        }
-
-        @Override
-        protected void after() {
-
-        }
-    };
+    public MojoRule rule = new MojoRule();
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
     /**
      * Test the basic running of the plugin.
+     *
      * @throws Exception if anything goes wrong.
      */
     @Test
     public void testDefaultSuccessfulRun() throws Exception {
-        final File pom = new File("target/test-classes/project-to-test/");
+        final File pom = new File("target/test-classes/default-test-project/");
         assertNotNull(pom);
         assertTrue(pom.exists());
 
@@ -50,18 +42,38 @@ public class GitBuildHookMojoTest {
 
     /**
      * Tests that the plugin fails the build when the project is not managed by Git.
+     *
      * @throws IOException if a temp project cannot be created for testing.
      */
     @Test(expected = MojoFailureException.class)
     public void testFailureFromLackingGitRepo() throws Exception
     {
-        Files.copy(Paths.get("target/test-classes/project-to-test/pom.xml"),
+        Files.copy(Paths.get("target/test-classes/default-test-project/pom.xml"),
                    Paths.get(folder.getRoot().getAbsolutePath() + "/pom.xml"));
 
         assertTrue(folder.getRoot().exists());
         final GitBuildHookMojo gitBuildHookMojo = (GitBuildHookMojo) rule.lookupConfiguredMojo(folder.getRoot(), "check");
         assertNotNull(gitBuildHookMojo);
         gitBuildHookMojo.execute();
+    }
+
+    /**
+     * Tests that a new repo is initialised if none exists and the initialise flag is configured.
+     *
+     * @throws IOException if a temp project cannot be created for testing.
+     */
+    @Test
+    public void testInitialiseNewGitRepo() throws Exception
+    {
+        Files.copy(Paths.get("target/test-classes/test-project-with-initialise-true/pom.xml"),
+                   Paths.get(folder.getRoot().getAbsolutePath() + "/pom.xml"));
+
+        assertTrue(folder.getRoot().exists());
+        final Verifier verifier = new Verifier(folder.getRoot().toString());
+        verifier.executeGoal("install");
+        verifier.verifyErrorFreeLog();
+        verifier.assertFilePresent(".git");
+        verifier.resetStreams();
     }
 }
 
