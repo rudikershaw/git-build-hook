fb8fddc79dc5d2d9cc0dbe5ac64b57a8bee4b33b
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2022-04-03 15:49:53 +0100

    #31 Add test that build passed with initialize goal in existing git repo.

--- src/test/java/com/rudikershaw/gitbuildhook/InitializeMojoTest.java
+++ src/test/java/com/rudikershaw/gitbuildhook/InitializeMojoTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.maven.it.Verifier;
+import org.eclipse.jgit.api.Git;
 import org.junit.Test;
 
 /** Unit and integration tests for the GitBuildHookMojo. */
@@ -22,7 +23,28 @@ public class InitializeMojoTest extends AbstractMojoTest {
 
         final File rootFolder = getFolder().getRoot();
         assertTrue(rootFolder.exists());
-        final Verifier verifier = getVerifier(rootFolder.toString());
+
+        verifyRepositoryInitializedWithoutErrors(rootFolder);
+    }
+
+    /**
+     * Tests that the initialize goal does not fail the build when a git repository is already present.
+     *
+     * @throws IOException if a temp project cannot be created for testing.
+     */
+    @Test
+    public void testInitialiseOnExistingRepo() throws Exception {
+        moveToTempTestDirectory("test-project-initialise", "pom.xml");
+
+        final File rootFolder = getFolder().getRoot();
+        assertTrue(rootFolder.exists());
+        Git.init().setDirectory(rootFolder).call();
+
+        verifyRepositoryInitializedWithoutErrors(rootFolder);
+    }
+
+    private void verifyRepositoryInitializedWithoutErrors(final File projectDir) throws Exception {
+        final Verifier verifier = getVerifier(projectDir.toString());
         verifier.executeGoal("install");
         verifier.verifyErrorFreeLog();
         verifier.assertFilePresent(".git");
